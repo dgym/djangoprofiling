f065cd53efad4807a5312449bd92eaa3e119ea84
Helen ST <helenst@gmail.com>
2012-10-23 10:31:20 +0100

    initial import (hotshot profiling)

---
+++ djangoprofile/middleware/profile.py
@@ -0,0 +1,49 @@
+import os.path
+import sys
+import time
+import hotshot
+import hotshot.stats
+from django.conf import settings
+from cStringIO import StringIO
+
+
+class ProfileMiddleware(object):
+    """
+    Displays hotshot profiling for any view.
+    http://yoursite.com/yourview/?prof
+
+    Add the "prof" key to query string by appending ?prof (or &prof=)
+    and you'll see the profiling results in your browser.
+    It's set up to only be available in django's debug mode,
+    but you really shouldn't add this middleware to any production configuration.
+    * Only tested on Linux
+    """
+    def process_request(self, request):
+        if settings.DEBUG and 'prof' in request.GET:
+            filename = 'profile-' + time.strftime("%Y%m%dT%H%M%S", time.gmtime()) + '.prof'
+            self.filename = os.path.join(settings.PROFILE_DIR, filename)
+            self.prof = hotshot.Profile(self.filename)
+
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        if settings.DEBUG and 'prof' in request.GET:
+            return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
+
+    def process_response(self, request, response):
+        if settings.DEBUG and 'prof' in request.GET:
+            self.prof.close()
+
+            out = StringIO()
+            old_stdout = sys.stdout
+            sys.stdout = out
+
+            stats = hotshot.stats.load(self.filename)
+            stats.sort_stats('time', 'calls')
+            stats.print_stats()
+
+            sys.stdout = old_stdout
+            stats_str = out.getvalue()
+
+            if response and response.content and stats_str:
+                response.content = "<pre>" + stats_str + "</pre>"
+
+        return response
