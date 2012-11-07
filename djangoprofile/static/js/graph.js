d169a4ded8a844fec1bd94ea247b3e7c51ac24f7
Helen ST <helenst@gmail.com>
2012-11-07 08:24:00 +0000

    Search nodes and centre results in viewport

---
+++ djangoprofile/static/js/graph.js
@@ -0,0 +1,88 @@
+define([], function() {
+
+    var Node = function($el) {
+        this.outlets = [];
+        this.inlets = [];
+        this.$el = $el;
+        this.text = this.text();
+
+        var parts = $('text', $el).map(function(){
+            return $(this).text();
+        });
+
+
+        this.name = parts[0];
+        /*
+        this.total_time_ratio = parts[1];
+        this.time_ratio = parts[2];
+        this.called = parts[3];
+        */
+    };
+
+    Node.prototype = {
+        text: function() {
+            return this.$el.text();
+        }
+    };
+
+    var Edge = function($el) {
+        this.from = null;
+        this.to = null;
+        this.$el = $el;
+    };
+
+    var Graph = function(svgdoc) {
+        this.edges = [];
+        this.nodes = [];
+        this.svgdoc = svgdoc;
+    };
+
+    Graph.prototype = {
+
+        index: function() {
+            var graph = this;
+
+            // Iterate over each edge
+            $('.edge', this.svgdoc).each(function() {
+
+                var edge = this;
+                var edge_id = edge.id.substr(4);
+
+                // Edge's title attribute contains ids of from and to nodes
+                var ends = $('title', edge).text().split('->');
+                var from_id = parseInt(ends[0],10);
+                var to_id = parseInt(ends[1],10);
+
+                // Create the node objects if necessary
+                if ( typeof graph.nodes[from_id] === 'undefined' ) {
+                    graph.nodes[from_id] = new Node($('#node'+from_id, graph.svgdoc));
+                }
+
+                if ( typeof graph.nodes[to_id] === 'undefined' ) {
+                    graph.nodes[to_id] = new Node($('#node'+to_id, graph.svgdoc));
+                }
+
+                edge = new Edge(edge);
+
+                // Connect everything up
+                edge.from = graph.nodes[from_id];
+                edge.to = graph.nodes[to_id];
+
+                graph.nodes[from_id].outlets.push(edge);
+                graph.nodes[to_id].inlets.push(edge);
+            });
+        },
+
+        search: function(query) {
+            return this.nodes.filter(function(obj) {
+                return obj.text.match(query);
+            });
+        }
+    };
+
+    return {
+        Graph: Graph,
+        Node: Node,
+        Edge: Edge
+    };
+});
--- djangoprofile/static/js/profile.js
+++ djangoprofile/static/js/profile.js
@@ -1,131 +1,119 @@
-var Node = function(obj) {
-    this.outlets = [];
-    this.inlets = [];
-    this.obj = obj;
-};
+require(["jquery", 
+         "graph", 
+         "vendor/jquery.svg",
+         "vendor/jquery.tmpl"],
 
-var Edge = function(obj) {
-    this.from = null;
-    this.to = null;
-    this.obj = obj;
-};
+function($, graph) {
 
-var Graph = function(svgdoc) {
-    this.edges = [];
-    this.nodes = [];
-    this.svgdoc = svgdoc;
-};
+    function restoreIntendedScale(svg) {
+        // Read in and split the viewBoxAttr
+        var viewBoxAttr = svg.root().attributes.viewBox;
+        if ( typeof viewBoxAttr !== 'undefined' ) {
+            var box = viewBoxAttr.value.split(' '),
+                x = box[0],
+                y = box[1];
+                w = box[2];
+                h = box[3];
 
-Graph.prototype = {
-
-    index: function() {
-        var graph = this;
-
-        // Iterate over each edge
-        $('.edge', this.svgdoc).each(function() {
-
-            var edge = this;
-            var edge_id = edge.id.substr(4);
-
-            // Edge's title attribute contains ids of from and to nodes
-            var ends = $('title', edge).text().split('->');
-            var from_id = parseInt(ends[0],10);
-            var to_id = parseInt(ends[1],10);
-
-            // Create the node objects if necessary
-            if ( typeof graph.nodes[from_id] === 'undefined' ) {
-                graph.nodes[from_id] = new Node($('#node'+from_id, graph.svgdoc));
-            }
-
-            if ( typeof graph.nodes[to_id] === 'undefined' ) {
-                graph.nodes[to_id] = new Node($('#node'+to_id, graph.svgdoc));
-            }
-
-            edge = new Edge(edge);
-
-            // Connect everything up
-            edge.from = graph.nodes[from_id];
-            edge.to = graph.nodes[to_id];
-
-            graph.nodes[from_id].outlets.push(edge);
-            graph.nodes[to_id].inlets.push(edge);
-        });
-    }
-};
-
-function restoreIntendedScale(svg) {
-    // Read in and split the viewBoxAttr
-    var viewBoxAttr = svg.root().attributes.viewBox;
-    if ( typeof viewBoxAttr !== 'undefined' ) {
-        var box = viewBoxAttr.value.split(' '),
-            x = box[0],
-            y = box[1];
-            w = box[2];
-            h = box[3];
-
-        // Apply these values to the 
-        svg.configure({"height": h, "width": w});
-    }
-}
-
-function indexGraph(svgdoc) {
-    var edges = [];
-    var nodes = [];
-
-    return {
-        edges: edges,
-        nodes: nodes
-    };
-}
-
-$(document).ready(function(){
-
-    // Keep the graph at 95% of viewport height
-    $(window).resize(function(){
-        $(".viewport").height($(window).height()*0.95); 
-    });
-    $(window).resize();
-
-    var $graph = $('.graph');
-    $graph.svg({
-        loadURL: $graph.data('href-svg'),
-        onLoad: function(svg) {
-            var root = svg.root();
-            var $graph = $('.graph', root);
-
-            restoreIntendedScale(svg);
-
-            var graph = new Graph(root);
-            graph.index();
-
-            $('.edge').click(function() {
-                var edge_id = this.id.substr(4);
-                var edge = graph.edges[edge_id];
-                console.log(edge.from.obj);
-                console.log(edge.to.obj);
-            });
-
-            $('.node').click(function() {
-                var node_id = this.id.substr(4);
-                var node = graph.nodes[node_id];
-                console.log('calls', node.outlets.length, 'things');
-                console.log('called by', node.inlets.length, 'things');
-            });
+            // Apply these values to the 
+            svg.configure({"height": h, "width": w});
         }
-    });
+    }
 
-    
-    /*
-    var scale = 1;
-    $(".zoomin").click(function() {
-        scale *= 2;
-        $graph.attr({ transform: "scale(" + scale + "," + scale + "); translate(0,0)"});
-    });
+    $(document).ready(function(){
 
-    $(".zoomout").click(function() {
-        scale /= 2;
-        $graph.attr({ transform: "scale(" + scale + "," + scale + "); translate(0,0)"});
+        // Keep the graph at 95% of viewport height
+        $(window).resize(function(){
+            $(".viewport").height($(window).height()*0.95); 
+        });
+        $(window).resize();
+
+        var $viewport = $('.viewport');
+        var $graph = $('.graph');
+        $graph.svg({
+            loadURL: $graph.data('href-svg'),
+            onLoad: function(svg) {
+                var root = svg.root();
+                var $graph = $('.graph', root);
+
+                restoreIntendedScale(svg);
+
+                var callgraph = new graph.Graph(root);
+                callgraph.index();
+
+                $('.edge').click(function() {
+                    var edge_id = this.id.substr(4);
+                    var edge = callgraph.edges[edge_id];
+                    console.log(edge.from.obj);
+                    console.log(edge.to.obj);
+                });
+
+                $('.node').click(function() {
+                    var node_id = this.id.substr(4);
+                    var node = callgraph.nodes[node_id];
+
+                    $('.info .current-node').text(node.name);
+                });
+
+                $('button.search').click(function() {
+                    var query = $('input[name=query]').val();
+                    var results = callgraph.search(query);
+                    $(".info .search-results").remove();
+                    $tmpl = $("#searchResultsTmpl");
+                    $tmpl.tmpl({
+                        results: 
+                            $(results).map(function() {
+                                return { 
+                                    name: this.name,
+                                    id: this.$el.attr('id')
+                                };
+                            })
+                    }).appendTo(".info");
+
+                    return false;
+                });
+
+                function centreInView($el, $graph) {
+                    // Centre horizontally
+                    var centreLeft = $graph.width()/2 - $el.width()/2;
+                    var actualLeft = $el.position().left;
+                    var moveLeft = actualLeft - centreLeft;
+
+                    var viewLeft = $viewport.scrollLeft();
+                    $viewport.scrollLeft(viewLeft + moveLeft);
+
+                    // Centre vertically
+                    var centreTop = $graph.height()/2 - $el.width()/2;
+                    var actualTop = $el.position().top;
+                    var moveTop = actualTop - centreTop;
+
+                    var viewTop = $viewport.scrollTop();
+                    $viewport.scrollTop(viewTop + moveTop);
+                }
+
+                $('.info').on('click', '.search-results li', function() {
+                    var node_id = $(this).data('node-id');
+                    var $node = $('#' + node_id);
+
+                    centreInView($node, $('.graph'));
+                });
+            }
+        });
+
+        
+        /*
+        var scale = 1;
+        $(".zoomin").click(function() {
+            scale *= 2;
+            $graph.attr({ transform: "scale(" + scale + "," + scale + "); translate(0,0)"});
+        });
+
+        $(".zoomout").click(function() {
+            scale /= 2;
+            $graph.attr({ transform: "scale(" + scale + "," + scale + "); translate(0,0)"});
+        });
+        */
+
     });
-    */
 
 });
